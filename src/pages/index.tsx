2a3b2e911527485b2b02e8a82845fc9665944946
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-04-29 08:03:48 -0300

    format response JSON data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,56 @@
-// SPA | SSR | SSG
+import { GetStaticProps } from "next";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { api } from "../services/api";
+import { converDurationToTimeString } from "../utils/convertDurationToTimeString";
 
-export default function Home(props) {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
   return (
     <>
       <h1>Index</h1>
-      <p>{ JSON.stringify(props.episodes) }</p>
+      <p>{JSON.stringify(props.episodes)}</p>
     </>
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      thumbnail: episode.thumbnail,
+      description: episode.description,
+      url: episode.file.url,
+      duration: Number(episode.file.duration),
+      durationString: converDurationToTimeString(Number(episode.file.duration)), 
+    };
+  });
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 4, // 4h em segundos
   };
-}
+};
