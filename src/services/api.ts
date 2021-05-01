17d4f7454d32027777d3f00980ce54adc8cf48c1
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-05-01 11:17:40 -0300

    environment port

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
+let port = process.env.PORT || 3000;
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/'
+  baseURL: "http://localhost:" + port,
 });
-
-
-
