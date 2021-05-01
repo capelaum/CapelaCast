db686ea8cca086fb03df18f75359702d7c090c82
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-05-01 13:09:37 -0300

    update api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 let port = process.env.PORT || 3333;
 
 export const api = axios.create({
-  baseURL: "http://localhost:" + port,
+  baseURL: "https://capelacast-server.herokuapp.com",
 });
