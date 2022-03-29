30d60416e7f46ec4c0aa3c64833186eec6eeda07
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-03-29 18:08:56 -0300

    style: dark

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,11 @@
-import format from "date-fns/format";
-import ptBR from "date-fns/locale/pt-BR";
-
-import styles from "./styles.module.scss";
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
+import styles from './styles.module.scss'
 
 export function Header() {
-  const currentDate = format(new Date(), "EEEEEE, d MMMM", { 
-    locale: ptBR,
-  });
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR
+  })
 
   return (
     <header className={styles.headerContainer}>
@@ -15,5 +14,5 @@ export function Header() {
       <p>O melhor para você ouvir, sempre</p>
       <span>{currentDate}</span>
     </header>
-  );
+  )
 }
