49766ab12bf6110141bad7f513f1c076d9cf69b7
capelaum <thecapellett@gmail.com>
2023-02-09 20:03:06 -0300

    🐛 fixes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ module.exports = {
     domains: ['storage.googleapis.com']
   },
   swcMinify: true,
-  target: 'serverless',
   version: 2,
   builds: [{ src: 'package.json', use: '@vercel/next' }]
 }
