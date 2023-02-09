abdbf9ca0d8e680d50b7433c97553ed294b804d5
capelaum <thecapellett@gmail.com>
2023-02-09 20:01:37 -0300

    🐛 fix: serverless

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   images: {
     domains: ['storage.googleapis.com']
   },
-  swcMinify: true
+  swcMinify: true,
+  target: 'serverless',
+  version: 2,
+  builds: [{ src: 'package.json', use: '@vercel/next' }]
 }
