db7723a9ff7680a873fa2dad0c308599d88fe331
capelaum <thecapellett@gmail.com>
2023-02-09 20:07:22 -0300

    🐛 next.config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@ module.exports = {
   images: {
     domains: ['storage.googleapis.com']
   },
-  swcMinify: true,
-  version: 2,
-  builds: [{ src: 'package.json', use: '@vercel/next' }]
+  swcMinify: true
 }
